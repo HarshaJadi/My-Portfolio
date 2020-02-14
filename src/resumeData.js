22377c0889e36e75221f17cee20a1a094190f40b
Harsha Jadi <Harsha@example.com>
2020-02-14 17:37:19 +0530

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,106 @@
+let resumeData = {
+    //"imagebaseurl":"",
+    "name": "HarshaJadi",
+    "role": "Student",
+    "linkedinId":"Harsha Jadi",
+    //"skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of frontend development and like to learn about new technologies, write technical articles or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/harsha-jadi-657403195/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/HarshaJadi",
+          "className":"fa fa-github"
+        },
+        /*{
+          "name":"skype",
+          "url":"",
+          "className":"fa fa-twitter"
+        }*/
+      ],
+    "aboutme":"I am currently a pre-final year student at Karnataka Lingayat Education Society and pursuing my BCA from here. I am a learner. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"India",
+    "website":"https://github.com/HarshaJadi",
+    "education":[
+      {
+        "UniversityName":"KLE's Bachelor of Computer Application, Hubballi ",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Apr",
+        "YearOfPassing":"2021",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "UniversityName":"Vidyaniketan PU Science College, Hubballi",
+        "specialization":"Some specialization",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      },
+      {
+        "CompanyName":"Some Company",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"Jan",
+        "YearOfLeaving":"2018",
+        "Achievements":"Some Achievements"
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"C"
+      },
+      {
+        "skillname":"C++"
+      },
+      {
+        "skillname":"Java"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
